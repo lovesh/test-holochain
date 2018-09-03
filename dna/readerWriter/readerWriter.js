774fc8aec1d126d8a13c8ff4a3874f61e1be3494
lovesh harchandani <lovesh.bond@gmail.com>
2018-09-03 21:42:32 +0530

    test links
    Signed-off-by: lovesh harchandani <lovesh.bond@gmail.com>

--- dna/readerWriter/readerWriter.js
+++ dna/readerWriter/readerWriter.js
@@ -7,15 +7,49 @@ function genesis() {
 }
 
 function holoTextWrite(text) {
-  return commit('holoText', text)
+  var hash = commit('holoText', text);
+  var me = App.Key.Hash;
+  debug("App key hash is" + me);
+  var link_hash = commit("holoLink",{    
+    Links: [
+      {Base: me, Link: hash, Tag: "holoText"}
+    ]
+  });
+  debug("Link hash is" + link_hash);
+  return hash;
 }
 
 function holoTextRead(hash) {
+  var r = get(hash, { GetMask: HC.GetMask.All });
+  debug(r);
   return get(hash, { Local: true })
 }
 
 function holoTextReadFromDHT(hash) {
+  var r = get(hash, {StatusMask: HC.Status.Default });
+  debug(r);
   return get(hash)
+}
+
+function getHoloTextLinks(base) {
+  debug("base is " + base);
+  var links = getLinks(base, "holoText");
+  debug(links);
+  return links.length;
+}
+
+function getAllLinks(dummy_arg) {
+  var result = query({
+    Return: {
+      Hashes: true,
+      Entries: true
+    },
+    Constrain: {
+      EntryTypes: ["holoLink"]
+    }
+  });
+  debug(result);
+  return 0;
 }
 
 function validateText(text) {
@@ -23,9 +57,18 @@ function validateText(text) {
 }
 
 function validateCommit(entry_type, entry) {
-  return validateText(entry)
+  if (entry_type == 'holoText') 
+    return validateText(entry)
+  return true;
 }
 
 function validatePut(entry_type, entry) {
-  return validateText(entry)
+  if (entry_type == 'holoText') 
+    return validateText(entry)
+  return true;
+}
+
+function validateLink() {
+  // This function is neccessary for links to work
+  return true;
 }
